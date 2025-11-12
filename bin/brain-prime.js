76f8865e3294828c326460230d807547cc59cf75
kathryn <ivanv.sasha24@mail.ru>
2025-11-12 11:00:24 +0300

    fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 import { generateNum, isPrime } from '../src/helpers/helpers.js';
 
+console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`)
+
 for (let i = 0; i < 3; i += 1) {
     const number = generateNum();
     const correctAnswer = isPrime(number) ? 'yes' : 'no';
