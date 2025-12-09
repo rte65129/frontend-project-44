354eaa2eb120e9c54ed0ce9d1f06edca19e5e119
Dimylya <mr.dima.buryak@gmail.com>
2025-12-09 15:18:10 +0300

    gggg

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greetUser from '../src/cli.js';
-import { generateNum, isEven } from '../src/games/helpers.js';
+import readlineSync from 'readline-sync'
+import greetUser from '../src/cli.js'
+import { generateNum, isEven } from '../src/games/helpers.js'
 
-const name = greetUser();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const name = greetUser()
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
 for (let i = 0; i < 3; i += 1) {
-  const number = generateNum();
-  console.log(`Question: ${number}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  const correctAnswer = isEven(number);
+  const number = generateNum()
+  console.log(`Question: ${number}`)
+  const userAnswer = readlineSync.question('Your answer: ')
+  const correctAnswer = isEven(number)
 
   if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${name}!`);
-    process.exit();
+    console.log('Correct!')
+  }
+  else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+    console.log(`Let's try again, ${name}!`)
+    process.exit()
   }
 }
 
-console.log(`Congratulations, ${name}!`);
+console.log(`Congratulations, ${name}!`)
